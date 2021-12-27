67148c5b834eb0c57d87e5b7fe62970d26bc2ea6
Nikasaih <gchaducnguyen@gmail.com>
2021-12-27 20:40:57 +0100

    ajout des dossier back

---
+++ pxback/src/main/java/com/example/pxback/PxbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pxback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PxbackApplication.class, args);
+	}
+
+}
---
+++ pxback/src/test/java/com/example/pxback/PxbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pxback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
